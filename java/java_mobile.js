8a6f4b1edfda44f0344c0ca3bef1e0ce66a827b4
mgal15 <146798535+mgal15@users.noreply.github.com>
2024-10-28 18:05:23 -0700

    Add files via upload

--- java/java_mobile.js
+++ java/java_mobile.js
@@ -1,41 +1,83 @@
 function enter_one(){
-    var answers = 1;
-    var correct_ans = 0;
-    var question = document.querySelector('input[name=q1]:checked');
-    if(question && question.value == 'r'+ answers){
-        correct_ans++;
-        document.getElementById('q1_r1').parentNode.style.color = "green";
-        document.getElementById('q1_r1').parentNode.style.fontWeight = 'bold';
-        document.getElementById('img1').src = "/images/walk.png";
-        document.getElementById('q1_r1').parentNode.textContent = "Bruce Nauman is correct!"
-        document.getElementById('q1_r2').parentNode.textContent = "He created this piece in 1989";
-        document.getElementById('q1_r4').parentNode.textContent = "Also is called the 'stairs to nowhere'";
-        document.getElementById('q1_r3').parentNode.textContent = "Title is Stadium Piece";
+    var answers = "bruce nauman";
+    var question = document.getElementById('q1');
+    if(answers == question.value){
+        document.getElementById('q1').style.fontWeight = "bold";
+        document.getElementById('q1').style.color = "green";
+        document.getElementById('q1').style.border = "1px green solid";
+        document.getElementById('next_button').style.display = "block";
+        document.getElementById('message').style.display = "inline";
+        document.getElementById('message').style.color = "green";
+        document.getElementById('message').textContent = "Correct! Great Job";
+    } else {
+        document.getElementById('message').style.display = "inline";
+        document.getElementById('message').style.color = "red";
+        document.getElementById('message').textContent = "Wrong, try again";
+        document.getElementById('q1').style.fontWeight = "bold";
+        document.getElementById('q1').style.color = "red";
+        document.getElementById('q1').style.border = "1px red solid";
     }
 }
 function enter_two(){
-    var answers = 3;
-    var question = document.querySelector('input[name=q2]:checked');
-    if(question && question.value == 'r'+ answers){
-        document.getElementById('q2_r1').parentNode.style.color = "green";
-        document.getElementById('q2_r1').parentNode.style.fontWeight = 'bold';
-        document.getElementById('img2').src = "/images/walk.png";
-        document.getElementById('q2_r1').parentNode.textContent = "Tom Otterness is correct!"
-        document.getElementById('q2_r2').parentNode.textContent = "He created this piece in 1999";
-        document.getElementById('q2_r4').parentNode.textContent = "He placed each figuring where he once sat";
-        document.getElementById('q2_r3').parentNode.textContent = "Title is Feats of Strength";
+    var answers = "tom otterness";
+    var question = document.getElementById('q2');
+    if(answers == question.value){
+        document.getElementById('q2').style.fontWeight = "bold";
+        document.getElementById('q2').style.color = "green";
+        document.getElementById('q2').style.border = "1px green solid";
+        document.getElementById('next_button').style.display = "block";
+        document.getElementById('message').style.display = "inline";
+        document.getElementById('message').style.color = "green";
+        document.getElementById('message').textContent = "Correct! Great Job";
+    } else {
+        document.getElementById('message').style.display = "inline";
+        document.getElementById('message').style.color = "red";
+        document.getElementById('message').textContent = "Wrong, try again";
+        document.getElementById('q2').style.fontWeight = "bold";
+        document.getElementById('q2').style.color = "red";
+        document.getElementById('q2').style.border = "1px red solid";
     }
 }
 function enter_three(){
-    var answers = 2;
-    var question = document.querySelector('input[name=q3]:checked');
-    if(question && question.value == 'r'+ answers){
-        document.getElementById('q3_r1').parentNode.style.color = "green";
-        document.getElementById('q3_r1').parentNode.style.fontWeight = 'bold';
-        document.getElementById('img3').src = "/images/walk.png";
-        document.getElementById('q3_r1').parentNode.textContent = "Richard Serra is correct!"
-        document.getElementById('q3_r2').parentNode.textContent = "He created this piece in 1979-80";
-        document.getElementById('q3_r4').parentNode.textContent = "Based on the confluence of college pedestrian traffic at the base of Red Square";
-        document.getElementById('q3_r3').parentNode.textContent = "The Title is Wrights Triangle";
+    var answers = "richard serra";
+    var question = document.getElementById('q3');
+    if(answers == question.value){
+        document.getElementById('q3').style.fontWeight = "bold";
+        document.getElementById('q3').style.color = "green";
+        document.getElementById('q3').style.border = "1px green solid";
+        document.getElementById('next_button').style.display = "block";
+        document.getElementById('message').style.display = "inline";
+        document.getElementById('message').style.color = "green";
+        document.getElementById('message').textContent = "Correct! Great Job";
+    } else {
+        document.getElementById('message').style.display = "inline";
+        document.getElementById('message').style.color = "red";
+        document.getElementById('message').textContent = "Wrong, try again";
+        document.getElementById('q3').style.fontWeight = "bold";
+        document.getElementById('q3').style.color = "red";
+        document.getElementById('q3').style.border = "1px red solid";
     }
+    
+}
+
+function enter_four(){
+    var answers = "lloyd hamrol";
+    var question = document.getElementById('q4');
+    if(answers == question.value){
+        document.getElementById('q4').style.fontWeight = "bold";
+        document.getElementById('q4').style.color = "green";
+        document.getElementById('q4').style.border = "1px green solid";
+        document.getElementById('next_button').style.display = "block";
+        document.getElementById('message').style.display = "inline";
+        document.getElementById('message').style.color = "green";
+        document.getElementById('message').textContent = "Correct! Great Job";
+    } else {
+        document.getElementById('message').style.display = "inline";
+        document.getElementById('message').style.color = "red";
+        document.getElementById('message').textContent = "Wrong, try again";
+        document.getElementById('q4').style.fontWeight = "bold";
+        document.getElementById('q4').style.color = "red";
+        document.getElementById('q4').style.border = "1px red solid";
+    }
+    
 }
